74cb0fa6c7ad647463c403bc9dc997a53ae01322
ilyapanov123 <89517673116g@gmail.com>
2025-12-13 12:23:16 +0300

    module3-task1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,3 @@
-// src/components/App.tsx
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './main/MainPage';
 import LoginPage from './login/LoginPage';
@@ -8,25 +6,23 @@ import OfferPage from './offer/OfferPage';
 import NotFoundPage from './not-found/NotFoundPage';
 import PrivateRoute from './PrivateRoute';
 
-const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage offersCount={312} />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route
-          path="/favorites"
-          element={
-            <PrivateRoute>
-              <FavoritesPage />
-            </PrivateRoute>
-          }
-        />
-        <Route path="/offer/:id" element={<OfferPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage offersCount={312} />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route
+        path="/favorites"
+        element={
+          <PrivateRoute>
+            <FavoritesPage />
+          </PrivateRoute>
+        }
+      />
+      <Route path="/offer/:id" element={<OfferPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
